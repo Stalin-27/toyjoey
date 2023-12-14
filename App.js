cff4bdf938044645dba8f921ff6e43bed4d70512
Stalin-27 <153793848+Stalin-27@users.noreply.github.com>
2023-12-14 11:21:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import LOGIN from './Components/login';
+import Navbar from './Components/Navbar';
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
